dd4e39778a029414c7e01e30ebde79d70cf65293
Muhammad Junaid <muhammadjunaid0035@gmail.com>
2025-07-30 18:27:16 +0500

    header and hero component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 import Newsletter from "@/components/Newsletter";
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import HeroSlider from "@/components/HeroSlider";
+import FeaturedStores from "@/components/FeaturedStores";
 
 export default function Home() {
   return (
     <main>
       {/* Other content here */}
-
+      <Header/>
+      <HeroSlider/>
+      <FeaturedStores/>
       <Newsletter />
       <Footer />
     </main>
